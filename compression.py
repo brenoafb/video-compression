3852992264dd41787480e36ccaf0e54de6b43661
brenoafb <breno.afb@gmail.com>
2021-05-15 14:38:11 -0300

    Fix compression, add decompression

--- compression.py
+++ compression.py
@@ -1,45 +1,34 @@
 import numpy as np
-from block_utils import blocks_to_frame
+from block_utils import blocks_to_frame, get_blocks, get_block_from_motion_vectors 
 from img_utils import scale_to_img
+import matplotlib.pyplot as plt
 
-def build_frame(frame, residual, motion_vectors, block_size):
+def compress_sequence(frames, block_size, delta):
   '''
-  Build frame i+1 from frame i
+  Given a sequence of frames, compress it into residuals
+  and motion vectors
   '''
-  blocks_per_column = frame.shape[0] // block_size
-  blocks_per_row = frame.shape[1] // block_size
-  coords = []
-  for i in range(blocks_per_column):
-      i1 = i * block_size
-      for j in range(blocks_per_row):
-          j1 = j * block_size
-          coords.append((i1, j1))
-    
-  blocks = []
-  for (motion_vector, (i1, j1)) in zip(motion_vectors, coords):
-      block = get_block_from_motion_vectors(frame, motion_vector, block_size, i1, j1)
-      blocks.append((block, (i1, j1)))
-  
-  next_frame = blocks_to_frame(blocks, frame.shape[0], frame.shape[1])
-  next_frame += residual
-
-  return next_frame
-
-def get_block_from_motion_vectors(frame, motion_vector, block_size, i1, j1):
-  '''
-  Find the corresponding block in frame i+1 from frame i
-  '''
-  i0 = i1 - motion_vector[0]
-  j0 = j1 - motion_vector[1]
-
-  block = frame[i0 : i0 + block_size, j0 : j0 + block_size, :]
-  return block
-
+  first_frame = frames[0]
+  prev_frame = first_frame
+  residuals = []
+  motion_vectors = []
+  for (i, curr_frame) in enumerate(frames[1:]):
+    print(i)
+    blocks = get_blocks(curr_frame, block_size)
+    residual, m, M, motion_vector = get_residual_and_vectors(prev_frame, blocks, delta)
+    # scaled_residual = residual.astype(np.float32)
+    # scaled_residual /= 255.0
+    # scaled_residual *= M
+    # scaled_residual += m
+    residuals.append((residual, m, M))
+    motion_vectors.append(motion_vector)
+    prev_frame = curr_frame
+  return (first_frame, residuals, motion_vectors)
 
 def get_residual_and_vectors(frame, blocks, delta):
   block_size = blocks[0][0].shape[0]
-  height, width= frame.shape
-  residuals = []
+  height, width, _ = frame.shape
+  block_residuals = []
   motion_vectors = []
   for (block, coords) in blocks:
       found_coords = get_matching_block(frame, block, coords[0], coords[1], delta)
@@ -47,12 +36,12 @@ def get_residual_and_vectors(frame, blocks, delta):
                           found_coords[1] : found_coords[1] + block_size,
                           :]
       motion_vector = (coords[0] - found_coords[0], coords[1] - found_coords[1])
-      residual = block.astype(np.float32) - found_block.astype(np.float32)
-      residuals.append((residual, coords))
+      block_residual = block.astype(np.float32) - found_block.astype(np.float32)
+      block_residuals.append((block_residual, coords))
       motion_vectors.append(motion_vector)
 
-  residual = scale_to_img(blocks_to_frame(residuals, height, width))
-  return (residual, motion_vectors)
+  (residual, m, M) = scale_to_img(blocks_to_frame(block_residuals, height, width))
+  return (residual, m, M, motion_vectors)
 
 def get_matching_block(mat: np.ndarray, block: np.ndarray, i0: int, j0: int, delta: int) -> np.ndarray:
     '''
--- img_utils.py
+++ img_utils.py
@@ -6,7 +6,9 @@ def scale_to_img(array):
     range [0,255]
     '''
     array = array.astype(np.float32)
-    array -= array.min()
-    array /= array.max()
+    m = array.min()
+    array -= m
+    M = array.max()
+    array /= M
     array *= 255
-    return array.astype(np.uint8)
+    return (array.astype(np.uint8), m, M)
