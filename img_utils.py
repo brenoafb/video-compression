9bf93bc22c1ab74ea11aa46008e3ad5baccf5a50
GiordanoSM <giordano@suffert.com>
2021-05-19 21:38:26 -0300

    Mudança no scaling dos resíduos

--- img_utils.py
+++ img_utils.py
@@ -6,22 +6,27 @@ def scale_to_img(array):
     range [0,255]
     '''
     array = array.astype(np.float32)
-    m = array.min()
-    array -= m
-    M = array.max() #Max value after the zero dislocation
-    array /= M
-    array *= 255
+    #m = array.min()
+    #array -= m
+    #M = array.max() #Max value after the zero dislocation
+    #array /= M
+    #array *= 255
+    array += 128
+    #array[array > 255] = 255
+    #array[array < 0] = 0
     array = np.around(array)
-    return (array.astype(np.uint8), m, M)
+
+    return (array.astype(np.uint8), 0, 0)
 
 def scale_from_img(residual, m, M):
     '''
     given an array and the values used to scale it, recover the original values
     '''
     scaled_residual = residual.astype(np.float32)
-    scaled_residual *= M
-    scaled_residual /= 255
-    scaled_residual += m
+    #scaled_residual *= M
+    #scaled_residual /= 255
+    #scaled_residual += m
+    scaled_residual -= 128
     scaled_residual = np.around(scaled_residual)
 
     return scaled_residual.astype(np.float32)
